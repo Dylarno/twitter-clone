c0cdb9f25fb49d979e6510da2d13fd389f8f9b09
Dylarno <medeirosdylan@hotmail.com>
2021-11-20 13:07:04 -0500

    More dummy data so the home page scrolls

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -21,6 +21,7 @@ export const Home = () => {
                 <Tweet avatar="https://media-exp1.licdn.com/dms/image/C4E03AQFjsfZFqe9eng/profile-displayphoto-shrink_800_800/0/1581445786326?e=1642636800&v=beta&t=Zyo9m3npzNYxhPY600iW61p60sXv9lAiYGoZ9g0blsg" displayname="Tomas" username="@toooooomas" tweetText="I really need to mute Mark on discord, he's sending me too many GIFs.." />
                 <Tweet avatar="https://pbs.twimg.com/profile_images/1268673564800139267/Nph2nyZ6_400x400.jpg" displayname="Dylan" username="@dylanmede_" tweetText="Off to work!" />
                 <Tweet avatar="https://pbs.twimg.com/profile_images/1268673564800139267/Nph2nyZ6_400x400.jpg" displayname="Dylan" username="@dylanmede_" tweetText="Tired.." />
+                <Tweet avatar="https://media-exp1.licdn.com/dms/image/C4D03AQHA9CvCb_ea4w/profile-displayphoto-shrink_800_800/0/1600375872048?e=1642636800&v=beta&t=x4IK-8Ih33mo7L32tzhJ3Mtf0LPQm4FD6XpMjPKFPKU" displayname="Mark" username="@markmcgregor" tweetText="La-La-La-La!" />
             </div>
         </div>
     );
